f1a2a469730976444d8b92c0c725d953800682ad
R. Ito <co.panda.fc3s@gmail.com>
2020-05-18 21:07:05 +0900

    spreadsheetIdを引数化

--- write_sheet.py
+++ write_sheet.py
@@ -1,9 +1,10 @@
 # -*- coding:utf-8 -*-
 import gsheet
+import os
 
 gsheet.write_data(
+  spreadsheet_id=os.environ.get('SPREADSHEET_ID'), # 環境計測情報記録用シートID
   values=gsheet.collect_data(), 
   service=gsheet.get_authentication(),
   range='sensor!A1'
 )
-
