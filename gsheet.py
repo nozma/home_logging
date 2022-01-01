b9f9c5bf0da856148f8c74f0538a5f36bbe9047b
nozma <co.panda.fc3s[at]gmail.com>
2022-01-01 12:01:36 +0900

    credentials.jsonを使うようにした

--- gsheet.py
+++ gsheet.py
@@ -3,8 +3,8 @@ from __future__ import print_function
 import pickle
 import os.path
 import datetime as dt
+from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 import remo
@@ -13,26 +13,9 @@ import co2
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 
 def get_authentication():
-  creds = None
-  # The file token.pickle stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-      creds = pickle.load(token)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', SCOPES)
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-      pickle.dump(creds, token)
-
-  service = build('sheets', 'v4', credentials=creds)
+  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+  service = build('sheets', 'v4', credentials=credentials)
   return service
 
 def write_data(spreadsheet_id, values, service, range):
